9ec84ceefa4d3ee9aeea0f4caa8c630b9b0b1e97
Walter Scheper <walter.scheper@gmail.com>
2020-11-30 21:06:03 -0500

    fix(newsfile): scan for start comments
    The original way `stentor` scanned for the start comment worked so long
    as the newline after the comment didn't align just after the end of the
    internal read buffer. To fix that, stentor now scans the end of the read
    buffer looking for partial matches with the start comment.
    
    Fixes #28
    
    (cherry picked from commit 327edd04efeac3e1a169a78494ae4f1b7247463a)

--- newsfile/newsfile.go
+++ newsfile/newsfile.go
@@ -27,7 +27,7 @@ const (
 )
 
 func WriteFragments(fn, startComment string, data []byte, keepHeader bool) error {
-	tf, err := writeFragments(fn, startComment, data, keepHeader)
+	tf, err := writeFragments(fn, []byte(startComment), data, keepHeader)
 	if err != nil {
 		return err
 	}
@@ -41,7 +41,7 @@ func WriteFragments(fn, startComment string, data []byte, keepHeader bool) error
 	return nil
 }
 
-func writeFragments(fn, startComment string, data []byte, keepHeader bool) (string, error) {
+func writeFragments(fn string, startComment, data []byte, keepHeader bool) (string, error) {
 	dst, err := ioutil.TempFile("", "")
 	if err != nil {
 		return "", err
@@ -71,7 +71,7 @@ func writeFragments(fn, startComment string, data []byte, keepHeader bool) (stri
 }
 
 // nolint:gocognit // try to simplify this at some point
-func copyIntoFile(dst io.Writer, src io.Reader, startComment string, data []byte, keepHeader bool) error {
+func copyIntoFile(dst io.Writer, src io.Reader, startComment, data []byte, keepHeader bool) error {
 	var partialBuf []byte = make([]byte, 0)
 	var startFound bool
 	for {
@@ -83,7 +83,9 @@ func copyIntoFile(dst io.Writer, src io.Reader, startComment string, data []byte
 		}
 
 		// trim off zero bytes if we under read
-		readBuf = readBuf[:n]
+		if n < readLength {
+			readBuf = readBuf[:n]
+		}
 
 		if len(partialBuf) > 0 {
 			// we had a partial read so prepend it to readBuf
@@ -93,7 +95,7 @@ func copyIntoFile(dst io.Writer, src io.Reader, startComment string, data []byte
 
 		if !startFound {
 			// find index of startComment
-			idx := bytes.Index(readBuf, []byte(startComment))
+			idx := bytes.Index(readBuf, startComment)
 			if idx >= 0 {
 				idx += len(startComment)
 				if keepHeader {
@@ -105,10 +107,12 @@ func copyIntoFile(dst io.Writer, src io.Reader, startComment string, data []byte
 				}
 				startFound = true
 			} else {
-				// trim off everything after the last full line
-				lastIdx := bytes.LastIndex(readBuf, []byte("\n"))
-				if lastIdx >= 0 && lastIdx < len(readBuf)-1 {
-					readBuf, partialBuf = readBuf[:idx+1], readBuf[idx+1:]
+				// check for sub-strings of the start comment at the end of the read
+				for i := len(startComment) - 1; i >= 0 && len(readBuf)-i > 0; i-- {
+					if bytes.Equal(startComment[:i], readBuf[len(readBuf)-i:]) {
+						readBuf, partialBuf = readBuf[:len(readBuf)-i], readBuf[len(readBuf)-i:]
+						break
+					}
 				}
 			}
 		}
