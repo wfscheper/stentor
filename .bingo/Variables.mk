da7f773f837f9857d084ec95713ca14e42feb4c7
Walter Scheper <ratlaw@gmail.com>
2025-01-02 23:03:56 -0500

    build: use stentor for tool management
    See https://github.com/bwplotka/bingo

---
+++ .bingo/Variables.mk
@@ -0,0 +1,42 @@
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.9. DO NOT EDIT.
+# All tools are designed to be build inside $GOBIN.
+BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+GOPATH ?= $(shell go env GOPATH)
+GOBIN  ?= $(firstword $(subst :, ,${GOPATH}))/bin
+GO     ?= $(shell which go)
+
+# Below generated variables ensure that every time a tool under each variable is invoked, the correct version
+# will be used; reinstalling only if needed.
+# For example for golangci-lint variable:
+#
+# In your main Makefile (for non array binaries):
+#
+#include .bingo/Variables.mk # Assuming -dir was set to .bingo .
+#
+#command: $(GOLANGCI_LINT)
+#	@echo "Running golangci-lint"
+#	@$(GOLANGCI_LINT) <flags/args..>
+#
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.63.3
+$(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.63.3"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.63.3 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+
+GORELEASER := $(GOBIN)/goreleaser-v1.26.2
+$(GORELEASER): $(BINGO_DIR)/goreleaser.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/goreleaser-v1.26.2"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=goreleaser.mod -o=$(GOBIN)/goreleaser-v1.26.2 "github.com/goreleaser/goreleaser"
+
+GOTAGGER := $(GOBIN)/gotagger-v0.9.1
+$(GOTAGGER): $(BINGO_DIR)/gotagger.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/gotagger-v0.9.1"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=gotagger.mod -o=$(GOBIN)/gotagger-v0.9.1 "github.com/sassoftware/gotagger/cmd/gotagger"
+
+GOTESTSUM := $(GOBIN)/gotestsum-v1.12.0
+$(GOTESTSUM): $(BINGO_DIR)/gotestsum.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/gotestsum-v1.12.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=gotestsum.mod -o=$(GOBIN)/gotestsum-v1.12.0 "gotest.tools/gotestsum"
